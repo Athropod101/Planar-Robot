c772ca2aa1805ee9742fbeda633851feba6eea66
Gabriel <gdiaz372003@proton.me>
2026-01-19 20:22:31 -0400

    Not going to integrate kinematics.py into sensor.py yet. It feels pointless, so I'll try to convince the professor of just that tomorrow.

--- simulator.py
+++ simulator.py
@@ -2,6 +2,8 @@ from sensor import Sensor
 from motor import Motor
 from kinematics import Kinematics
 
+SampleTime = 0.1
+
 MotorData = {
         'Torque'    : 0.15,     # Nm
         'Resistance': 0.0001139,# Ohm
@@ -15,11 +17,18 @@ RobotData = {
         'Differential': 0.1  # m
         }
 
+InitialPosition = np.array([
+    [0],
+    [0],
+    [0]
+    ]
+
 SensorData = {
         'NoiseMean': 0 # rad/s
         'NoiseDev': 10 # percentage
         }
 
+RobotMotion = Kinematics(RoboData, InitialPosition, SampleTime)
 
 LeftMotor = Motor(MotorData)
 RightMotor = Motor(MotorData)
